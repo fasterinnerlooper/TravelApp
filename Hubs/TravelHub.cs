c09b96b85dff26cf4f5038bac93c95a7a0f874ca
sjetha <shafiq.jetha@gmail.com>
2014-06-08 22:40:41 -0600

    Restarted project using SignalR and knockoutjs for interactivity

---
+++ Hubs/TravelHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+
+namespace BanjiTravelApp.Hubs
+{
+    public class TravelHub : Hub
+    {
+        public void AddMarker(dynamic lat, dynamic lng)
+        {
+            Clients.All.successful(lat, lng);
+        }
+    }
+}
