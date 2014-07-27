7925dfe5b0d451f923777aea51e1123fcf0c5d7d
Shafiq <shafiq.jetha@outlook.com>
2014-07-26 22:09:52 -0600

    Building up to getting Travel Plans working

---
+++ BanjiTravelApp/Models/Tenses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanjiTravelApp.Models
+{
+    public enum Tenses
+    {
+        Past,
+        Present,
+        Future
+    }
+}
--- BanjiTravelApp/Scripts/app/travelPlans.viewmodel.js
+++ BanjiTravelApp/Scripts/app/travelPlans.viewmodel.js
@@ -1,12 +1,22 @@
 ﻿function TravelPlansDataModel() {
     var self = this;
-    self.url = "/travelPlans/";
+    self.url = "api/travelPlans/";
     
-
     self.plans = null;
 
-    self.getPlans() = function () {        
+    self.getPlans() = function () {
         return $.ajax(profileInfo + self.username);
     }
 
-}
+    self.showAllPastMarkers = function () {
+        $.ajax(self.uri, {
+            type: "GET",
+            data: {
+                tense: 'past',
+                date: (new Date()).toJSON()
+            }
+        }).success(function (data) {
+            console.dir(data);
+        })
+    }
+};
